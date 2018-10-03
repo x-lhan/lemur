9727db2f7f14e2dc02b7b0952cd193a71279529f
Lu <lhan@xetus.com>
2018-10-03 14:51:30 -0700

    forgot to modify setup.py

--- setup.py
+++ setup.py
@@ -154,6 +154,7 @@ setup(
             'digicert_cis_issuer = lemur.plugins.lemur_digicert.plugin:DigiCertCISIssuerPlugin',
             'digicert_cis_source = lemur.plugins.lemur_digicert.plugin:DigiCertCISSourcePlugin',
             'csr_export = lemur.plugins.lemur_csr.plugin:CSRExportPlugin',
+            'vault_issuer = lemur.plugins.lemur_vault.plugin:VaultIssuerPlugin',
             'sftp_destination = lemur.plugins.lemur_sftp.plugin:SFTPDestinationPlugin'
         ],
     },
